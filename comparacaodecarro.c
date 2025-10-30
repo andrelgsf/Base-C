834a8b648e06ca878016b127109f1d266d00e1bb
André Luiz <141333358+andrelgsf@users.noreply.github.com>
2025-10-30 08:31:25 -0300

    Add files via upload

---
+++ comparacaodecarro.c
@@ -0,0 +1,30 @@
+//cria um estrutura "produto" e calcula o valor total em estoque(preço x quantidade) de 3 produtos
+
+#include <stdio.h>
+
+typedef struct {
+    char nome[50];
+    int quantidade;
+    float preco;
+} Produto;
+
+int main() {
+    float estoque;
+    Produto produto;
+
+ printf("Digite o nome do produto \n");
+ scanf("%s", produto.nome);
+
+
+ printf("Digite a quantidade do produto \n");
+ scanf("%d", &produto.quantidade);
+
+ printf("Digite o preço do produto\n ");
+ scanf("%f", &produto.preco);
+
+ estoque = produto.preco * produto.quantidade;
+
+ printf("O estoque do produto %s é de %.0f\n ", produto.nome, estoque);
+
+    return 0;
+}
